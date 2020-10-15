e82f04fc88afef09745e256ec47808d84f5d8be2
Lucie Molinie <lucie.molinie@telecom-paris.fr>
2020-10-15 18:29:37 +0200

    Updates

---
+++ precache-manifest.a739994993adcc50ad62dbaeb7a8e4a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7f64c9f97c922f6fdd48fe394c8c39",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "9905406d4905cacbbaf8",
+    "url": "/blindtest-spotify/static/css/main.348ad1c0.chunk.css"
+  },
+  {
+    "revision": "99291f00ffc51cf5bd8a",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js"
+  },
+  {
+    "revision": "8dfbd160926ae814ef4bd67a82331fc7",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9905406d4905cacbbaf8",
+    "url": "/blindtest-spotify/static/js/main.429c18ce.chunk.js"
+  },
+  {
+    "revision": "01c4d54207b3c1322762",
+    "url": "/blindtest-spotify/static/js/runtime-main.e8875b69.js"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  }
+]);
